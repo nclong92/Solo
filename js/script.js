5af2b81423a87485c17089a36b7d4d7ed20c4842
longnc <nclong92@gmail.com>
2019-11-11 17:14:15 +0700

    team part 1 ...

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // preloader
-$(window).on('load', function(){
+$(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut("slow");
 })
+
+// Team
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
+
+// Progress Bars
+jQuery(document).ready(function(){
+    jQuery
+});
