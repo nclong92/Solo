66221e6bd25b3715aee48f5e582f10148776e6ee
longnc <nclong92@gmail.com>
2019-11-19 17:28:31 +0700

    add jquery filter part 2 ...

--- js/script.js
+++ js/script.js
@@ -42,3 +42,26 @@ $().ready(function(){
         animation: 'slide'
     });
 });
+
+// Portfolio
+$(window).on('load', function(){
+
+    // Initialize Isotope
+    $("#isotope-container").isotope({
+
+    });
+
+    // filter items on button click
+    $("#isotope-filters").on('click', 'button', function(){
+        // get filter value
+        var filterValue = $(this).attr('data-filter');
+
+        // filter portfolio
+        $("#isotope-container").isotope({
+            filter: filterValue
+        });
+
+        $("#isotope-filters").find('.active').removeClass('active');
+        $(this).addClass('active');
+    })
+});
